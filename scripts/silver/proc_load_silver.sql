ec6eddd6ef6978f28fb2c7dd65f0ea6be62b69c9
Heyshrith Kondala <kheyshrith@gmail.com>
2026-09-13 14:05:13 +0530

    Update documentation for proc_load_silver
    Added usage example and actions performed to the stored procedure documentation.

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -1,6 +1,9 @@
 /*
     This stored procedure performs the ETL (Extract, Transform, Load) process to 
-    populate the 'silver' schema tables from the 'bronze' schema.
+populate the 'silver' schema tables from the 'bronze' schema.
+Actions Performed:
+	- Truncates Silver tables.
+	- Inserts transformed and cleansed data from Bronze into Silver tables.
 
 Usage Example:
     EXEC Silver.load_silver;
